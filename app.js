1031c55692df15fd47bcbef34fc8dfacbd2c895c
swetarajak <swetarajak001@gmail.com>
2025-02-13 22:22:40 +0530

    Main app.js updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import { fileURLToPath } from 'url'; // Import this utility
 
 const app = express();
 const port = 3000;
@@ -11,8 +12,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
+const __filename = fileURLToPath(import.meta.url); // Get the current file's path
+const __dirname = path.dirname(__filename); // Extract the directory
+
 // Serve static files (CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Use the password routes
+app.use('/', passwordRoutes);
+
 
 // Start the server
 app.listen(3000, () => {
