e21632d5378843cddc795a3793168fd4d426f89d
swetarajak <swetarajak001@gmail.com>
2025-02-14 12:44:41 +0530

    App.js updated

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url'; // Import this utility
-import { passwordRoutes } from "./Routes/password.js";
+import passwordRoutes  from "./Routes/passwordRoutes.js";
 
 const app = express();
 const port = 3000;
 
+
+// Middleware
+app.use(express.json()); // Parses incoming JSON requests
 // Middleware for parsing form data
 app.use(bodyParser.urlencoded({extended: true}));
 
